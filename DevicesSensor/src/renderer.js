75595df67eee04d1d1cfc4070ca4f024f3f3730a
show <d0r5i5z1z8l0e@gmail.com>
2022-10-24 20:03:41 +0800

    clean superfluous function

--- DevicesSensor/src/renderer.js
+++ DevicesSensor/src/renderer.js
@@ -2,18 +2,17 @@ let keyPressed = {};
 
 //read setting config json file
 let Setting_config_map = new Map();
-Setting_config_map.set("CPU_info",false);
-Setting_config_map.set("CPU_Temp",false);
-Setting_config_map.set("CPU_Usage",false);
 
 const network = document.getElementById("Network");
 const CPU = document.getElementById("cpu");
 const GPU = document.getElementById("gpu");
 const settingObject = document.getElementById("setting");
 
-
+setInterval(CPU_update,3000);
 setInterval(ram_update,3000);
 setInterval(GPU_update,3000);
+setInterval(Net_update,3000);
+setInterval(FPS_Update,3000);
 settingObject.addEventListener('click', ()=>{
     window.api.openSettingWindow();
 });
@@ -124,19 +123,3 @@ async function FPS_Update(){
 document.addEventListener('keyup', (e) => {
     delete keyPressed[e.key];
 });*/
-
-
-//split every system status, use remove and add setInterval method
-async function Update_sys_status () {
-    const CPU_status = await window.api.getCPUStatus();
-    const GPU_status = await window.api.getGPUStatus();
-    const CPU_Use = CPU_status.get("CPUUse");
-    const CPU_temp = CPU_status.get("CPUTemp");
-    const Net_speed = await window.api.getNetworkStatus();
-
-
-    GPU.innerText = ` ${GPU_status.get("GPUuse").toFixed(1)}%,${GPU_status.get("GPUtemp")}℃`;
-    CPU.innerText = ` ${CPU_Use.toFixed(1)}%,${CPU_temp}℃ `;
-    network.innerText = `${Net_speed} Mbit/s`;
-
-}
